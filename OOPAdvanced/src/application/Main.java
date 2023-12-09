f011dd733eee690a8a8ead5d9066df7c6cb6a79a
Hancovirus <22028081@vnu.edu.vn>
2023-12-09 22:10:31 +0700

    Add Project

---
+++ OOPAdvanced/src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage stage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Ui.fxml"));
+			Scene scene = new Scene(root);
+			String css = this.getClass().getResource("application.css").toExternalForm();
+			scene.getStylesheets().add(css);
+			
+			stage.setTitle("OOP Advanced");
+		    
+		    stage.setScene(scene);
+		    stage.setResizable(false);
+		    stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(final String[] args) { 
+		launch(args);
+	}
+}
